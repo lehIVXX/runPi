951b4912f2edc6dfeec7ccb12ca076f4dbd4f428
lehIVXX <101026134+lehIVXX@users.noreply.github.com>
2024-01-17 14:55:16 +0200

    Create mylcd.py

---
+++ mylcd.py
@@ -0,0 +1,20 @@
+from subprocess import check_output
+from time import sleep
+from datetime import datetime
+from rpi_lcd import LCD
+lcd = LCD()
+lcd.clear()
+def get_ip():
+    cmd = "hostname -I | cut -d\' \' -f1"
+    return check_output(cmd, shell=True).decode("utf-8").strip()
+while True:
+    lcd_line_1 = datetime.now().strftime('%b %d - %H:%M')
+    lcd_line_2 = "IP " + get_ip()
+    lcd_line_3 = "Web octopi.local"
+    lcd.clear()
+    lcd.text(lcd_line_1,2)
+    lcd.text(lcd_line_2,2)
+    sleep(30)
+    lcd.text(lcd_line_3,2)
+    sleep(30)
+
